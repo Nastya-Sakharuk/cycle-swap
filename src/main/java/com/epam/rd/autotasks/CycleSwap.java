550b6d61c5a27fd77c51b2e5fab687b9f311e6fa
Anastasiya Sakharuk <sakharuk333@gmail.com>
2022-11-30 20:53:15 +0300

    Cycle Swap

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,11 +1,21 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 class CycleSwap {
-    static void cycleSwap(int[] array) {
-
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 3, 2, 7, 4};
+        int shift = 3;
+        cycleSwap(array, shift);
+        System.out.println(Arrays.toString(array));
     }
-
+    static void cycleSwap(int[] array) {
+            cycleSwap(array, 1);
+    }
     static void cycleSwap(int[] array, int shift) {
-
+        int[] arr2 = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arr2[(array.length + i - shift) % array.length];
+        }
     }
 }
